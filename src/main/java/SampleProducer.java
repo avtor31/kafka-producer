3e4e7d3158b2915b07d95b351ad99e040b620a84
avtor31 <avtor31@yandex.ru>
2020-03-09 18:19:20 +0300

    Producer and Runner was created

---
+++ src/main/java/SampleProducer.java
@@ -0,0 +1,19 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SampleProducer {
+
+    public SampleProducer() {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("channel", "name", "some sent data");
+        KafkaProducer kafkaProducer = new KafkaProducer(properties);
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.close();
+    }
+}
